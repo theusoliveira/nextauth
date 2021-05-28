1a75823510c11add11e6566721598e766ab662aa
Matheus Oliveira <matheus.oliveira@ivoryit.com.br>
2021-05-28 14:26:57 -0300

    Refresh token

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,13 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
+import { api } from '../services/api';
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    api.get('/me').then(res => console.log(res.data));
+  }, []);
 
   return <h1>Bem vindo, {user?.email}</h1>;
 }
